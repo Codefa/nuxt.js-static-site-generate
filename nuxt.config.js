dae03ae9d62f40d3556285a8aa8d8fd255f62de5
SHARON <sharushj690@gmail.com>
2018-09-24 22:37:53 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,26 +28,7 @@ module.exports = {
 
   // Generate dynamic routes
   generate: {
-    minify: {
-  collapseBooleanAttributes: true,
-  collapseWhitespace: false,
-  decodeEntities: true,
-  minifyCSS: true,
-  minifyJS: true,
-  processConditionalComments: true,
-  removeAttributeQuotes: false,
-  removeComments: false,
-  removeEmptyAttributes: true,
-  removeOptionalTags: false,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: false,
-  removeStyleLinkTypeAttributes: false,
-  removeTagWhitespace: false,
-  sortAttributes: true,
-  sortClassName: false,
-  trimCustomFragments: true,
-  useShortDoctype: true
-},
+    minify: { removeAttributeQuotes: true },
     async routes() {
       const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
       return data.map(user => `/users/${user.id}`)
